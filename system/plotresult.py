ffaed496d66613489bd2bd0b5e479a933ebb8cb4
nick <vandykenick5@gmail.com>
2024-04-20 16:28:39 -0700

    add comments to the plotting script

--- system/plotresult.py
+++ system/plotresult.py
@@ -3,11 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.collections import PatchCollection
 
+#read the state file and return the values
 def readresult(filename):
     with open(filename,'r') as f:
         lines = f.readlines()
     arrayholder = []
     for line in lines:
+        #we are done when there is an empty line
         line = line.strip()
         if not line:
             continue
@@ -16,11 +18,13 @@ def readresult(filename):
     
     return result
 
+#read the error file and return the values
 def readerror(filename):
     with open(filename,'r') as f:
         lines = f.readlines()
     vals = []
     for line in lines:
+        #we are done on an empty line or non-number
         line = line.strip()
         if line == '':
             break
@@ -30,36 +34,51 @@ def readerror(filename):
         vals.append(line)
     return vals
 
+#normalize the data for plotting, so the colorscale lines up nicely
 def normalize(data):
     return (data-np.min(data))/(np.max(data)-np.min(data)) 
 
+#plot vals over the mesh
 def plotPDE(mesh,vals):
+    
+    #set up the plot
     fig = plt.figure()
     ax = fig.add_subplot(111)
     colorMap = plt.get_cmap('jet')
-    
+    patches = []
+
+    #normalize
     vals = normalize(vals)
 
-
-    patches = []
+    #loop over all elements
     for index, triangle in enumerate(mesh['E']):
+
+        #put all the x,y points in one array
         constructed = []
         for vertex in triangle:
             constructed.append(mesh['V'][vertex])
         constructed = np.array(constructed)
         #print(constructed)
-        
+
+        #create a polygon plot object with the associated value
         tri = plt.Polygon(constructed,color=colorMap(vals[index]))
         patches.append(tri)
         #plt.gca().add_patch(tri)
-    ax.add_collection(PatchCollection(patches, match_original=True))
-    ax.set_xlim([-2,2])
-    ax.set_ylim([-.5,1.5])
 
+    #plot all elements with their values
+    ax.add_collection(PatchCollection(patches, match_original=True))
+
+    #format plot
+    ax.set_xlim([np.min(mesh['V'][:,0]),np.max(mesh['V'][:,0])])
+    ax.set_ylim([np.min(mesh['V'][:,1]),np.max(mesh['V'][:,1])])
+    ax.set_aspect('equal', adjustable='box')
+
+#plot the error history
 def ploterror(error):
+
+    #create plot
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    
     plt.semilogy(error)
     plt.plot([0,len(error)],[1e-7]*2)
     plt.xlabel('Iteration')
@@ -67,7 +86,7 @@ def ploterror(error):
 
 resultfile = 'u.txt'
 errorfile = 'e.txt'
-meshfile = 'meshes\\kbump2.gri'
+meshfile = 'meshes\\bump0.gri'
 
 mesh = readgri(meshfile)
 result = readresult(resultfile)
